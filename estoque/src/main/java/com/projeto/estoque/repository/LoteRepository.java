68c9d2a3c54ec424414d7389c810f7ba6f50573b
nickpolo99 <160550179+nickpolo99@users.noreply.github.com>
2025-12-02 15:53:17 -0300

    Delete estoque directory

--- estoque/src/main/java/com/projeto/estoque/repository/LoteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.projeto.estoque.repository;
-import com.projeto.estoque.model.Lote;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface LoteRepository extends JpaRepository<Lote, Long> {
-    List<Lote> findByProdutoIdOrderByValidadeAsc(Long produtoId);
-}
--- estoque/src/main/java/com/projeto/estoque/repository/ProdutoRepository.java
+++
@@ -1,5 +0,0 @@
-package com.projeto.estoque.repository;
-import com.projeto.estoque.model.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> { }
